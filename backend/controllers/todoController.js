ff60425af1ce3fee355078b3b83aa828f8e1c8a9
Moses Fawole <mosesfawole2001@gmail.com>
2022-06-24 08:38:10 +0100

    added error middleware

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,28 +1,33 @@
 // @desc Get todos
 // @route GET /api/todos
 // @access Private
-const getTodos = (req, res) => {
+const getTodos = async (req, res) => {
   res.status(200).json({ message: "Get todos" });
 };
 
 // @desc Set todos
 // @route POST /api/todos
 // @access Private
-const setTodo = (req, res) => {
-  res.status(200).json({ message: "Get todos" });
+const setTodo = async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
+  res.status(200).json({ message: "Set todo" });
 };
 
 // @desc Uodate todos
 // @route PUT /api/todos
 // @access Private
-const updateTodo = (req, res) => {
+const updateTodo = async (req, res) => {
   res.status(200).json({ message: `Update todo ${req.params.id}` });
 };
 
 // @desc Delete todos
 // @route DELETE /api/todos
 // @access Private
-const deleteTodo = (req, res) => {
+const deleteTodo = async (req, res) => {
   res.status(200).json({ message: `Delete todo ${req.params.id}` });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
 const todoRoutes = require("./routes/todoRoutes");
 
 app.use("/api/todos", todoRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
