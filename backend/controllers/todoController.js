c4b0a751a6d0ab820dad2834b574eca54defe47d
Moses Fawole <mosesfawole2001@gmail.com>
2022-06-24 08:40:58 +0100

    Todos controllers and routes setup

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,35 +1,36 @@
+const asyncHandler = require("express-async-handler");
 // @desc Get todos
 // @route GET /api/todos
 // @access Private
-const getTodos = async (req, res) => {
+const getTodos = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get todos" });
-};
+});
 
 // @desc Set todos
 // @route POST /api/todos
 // @access Private
-const setTodo = async (req, res) => {
+const setTodo = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
 
   res.status(200).json({ message: "Set todo" });
-};
+});
 
 // @desc Uodate todos
 // @route PUT /api/todos
 // @access Private
-const updateTodo = async (req, res) => {
+const updateTodo = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update todo ${req.params.id}` });
-};
+});
 
 // @desc Delete todos
 // @route DELETE /api/todos
 // @access Private
-const deleteTodo = async (req, res) => {
+const deleteTodo = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete todo ${req.params.id}` });
-};
+});
 
 module.exports = {
   getTodos,
